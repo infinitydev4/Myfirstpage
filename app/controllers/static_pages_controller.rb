7f711d48fac41c5a6e67b88e56e5d2afe9fe5dab
infinitydev4 <infinitydev4@gmail.com>
2018-07-31 11:25:57 -0700

    init

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,17 @@
+class StaticPagesController < ApplicationController
+
+    def home
+    end
+
+    def about
+    end
+
+    def contact
+    end
+
+    def about_me
+    end
+
+    def about_us
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: 'static_pages#home', as: 'home'
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/about_me', to: 'static_pages#about', as: 'about_me'
+  get '/about_us', to: 'static_pages#about', as: 'about_us'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
